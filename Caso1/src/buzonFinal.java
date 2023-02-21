e8fbf7967594c0115aae0234ff24de3212e08bed
Daniel7578 <88909405+Daniel7578@users.noreply.github.com>
2023-02-20 22:06:43 -0500

    Caso 1 error eclipse

---
+++ Caso1/src/buzonFinal.java
@@ -0,0 +1,10 @@
+package ejecucion;
+
+public class buzonFinal extends buzon{
+
+    public buzonFinal(int cant) {
+        super(cant);
+    }
+
+    
+}
---
+++ Caso1/src/rojo.java
@@ -0,0 +1,9 @@
+package threads;
+
+public class rojo extends Thread{
+	private int red;
+	public void run()
+	{
+		
+	}
+}
