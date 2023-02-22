5940e6bc650a4a351b7409a2dc7dec68895ed04e
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-02-22 16:49:09 -0500

    Create Naranja.java
    naranja

---
+++ Caso_1/src/threads/Naranja.java
@@ -0,0 +1,58 @@
+package threads;
+
+import ejecucion.Buzon;
+import ejecucion.Identificador;
+import ejecucion.Producto;
+
+public class Naranja extends Thread{
+	private Buzon buzonEntrada;
+	private Buzon buzonSalida;
+	private int productos;
+	private int etapa;
+	private Producto productoEnProceso;
+	private Identificador identificador;
+	
+	public Naranja(Buzon buzonEntada, Buzon buzonSalida, int productos, int etapa, Producto productoEnProceso, Identificador identificador)
+	{
+		this.buzonEntrada = buzonEntada;
+		this.buzonSalida = buzonSalida;
+		this.productos = productos;
+		this.etapa = etapa;
+		this.productoEnProceso = productoEnProceso;
+		this.identificador = identificador;
+	}
+	
+	@Override
+	
+	public void run()
+	{
+		boolean termino = false;
+		while(!termino)
+		{
+			if(etapa == 1)
+			{
+				while(productos > 0)
+				{
+					Naranja.yield();
+				}
+				Producto prodE = buzonEntrada.retirarNarnja();
+				System.out.println("El proceso recibió el producto: " + prodE);
+				crearProducto();
+				System.out.println(productoEnProceso.getIdentificador());
+				productos--;
+				if(productos == 0)
+					termino = true;
+			}
+		}
+		
+	}
+	
+	public void crearProducto()
+	{
+		synchronized(buzonSalida)
+		{
+			productoEnProceso = new Producto(identificador.getIdActual(), "Creacion de Producto naranja en Etapa 1", 0);
+			identificador.sumIdActual();
+		}
+	}
+}
